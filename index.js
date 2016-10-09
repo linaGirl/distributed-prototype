6af90c543fc32a97870e34bad60c632d0898ebc6
Michael van der Weg <michael@joinbox.com>
2016-10-09 23:04:42 +0200

    fixed index.js

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 
 
 	module.exports = {
-        a: require('./src/ShoppingService');
+          FilterBuilder                 : require('./src/FilterBuilder')
+        , RelatedResourceController     : require('./src/RelatedResourceController')
+        , RelatedService                : require('./src/RelatedService')
+        , RelationalRequest             : require('./src/RelationalRequest')
+        , RelationalResourceController  : require('./src/RelationalResourceController')
+        , RelationalResponse            : require('./src/RelationalResponse')
+        , RelationalSelection           : require('./src/RelationalSelection')
+        , RelationalService             : require('./src/RelationalService')
+        , Request                       : require('./src/Request')
+        , ResourceController            : require('./src/ResourceController')
+        , Response                      : require('./src/Response')
+        , SelectionBuilder              : require('./src/SelectionBuilder')
+        , Service                       : require('./src/Service')
+        , ServiceBridge                 : require('./src/ServiceBridge')
     }
